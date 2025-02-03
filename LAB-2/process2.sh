d9aa6385bba5ea0a3611334e2673809b7577e195
Girish Kailash <girishseervi891@gmail.com>
2025-02-03 14:51:52 +0530

    Delete LAB-2/process2.sh

--- LAB-2/process2.sh
+++
@@ -1,6 +0,0 @@
-# Display the jobs running in the background
-echo -e "\nListing jobs:"
-jobs
-# Check if the process is running using ps
-echo -e "\nChecking if the task is running..."
-ps -p $task_pid
